430fb180c20ce00841144c65fce736af6237923f
alidarovolj <alidarovolzhas@gmail.com>
2024-10-03 00:39:16 +0500

    done with userProfile

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -122,7 +122,7 @@ const filteredLinks = computed(() => {
 
 onMounted(async () => {
   await nextTick()
-  if(localStorage.getItem('token')) {
+  if (localStorage.getItem('token')) {
     await users.getProfile();
   }
 });
@@ -201,9 +201,11 @@ onMounted(async () => {
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
             <li>
               <ul role="list" class="-mx-2 space-y-1">
-                <li>
-                  <RouterLink :to="navigation[0].href"
-                              :class="[route.name === navigation[0].alias ? 'bg-gray-50 text-mainColor' : 'text-gray-700 hover:bg-gray-50 hover:text-mainColor', 'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6']">
+                <li v-if="userProfile">
+                  <RouterLink
+                      v-if="userProfile.role.code !== 'admin'"
+                      :to="navigation[0].href"
+                      :class="[route.name === navigation[0].alias ? 'bg-gray-50 text-mainColor' : 'text-gray-700 hover:bg-gray-50 hover:text-mainColor', 'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6']">
                     <component :is="navigation[0].icon"
                                :class="[route.name === navigation[0].alias ? 'text-mainColor' : 'text-gray-400 group-hover:text-mainColor', 'h-6 w-6 shrink-0']"
                                aria-hidden="true"/>
